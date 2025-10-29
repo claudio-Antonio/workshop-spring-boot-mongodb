b0e12a4d32190fae07c456c5b84253318b36c249
Cláudio Antônio <claudioanery@gmail.com>
2025-10-29 15:53:31 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/claudioantonio/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.claudioantonio.workshopmongo.repositories;
+
+import com.claudioantonio.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
