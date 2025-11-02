24d455ff4301fb0bc2e8cfb3ea85a302cc69cb21
Cláudio Antônio <claudioanery@gmail.com>
2025-11-02 10:18:37 -0300

    Atualização de usuário com PUT

--- src/main/java/com/claudioantonio/workshopmongo/services/UserService.java
+++ src/main/java/com/claudioantonio/workshopmongo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -29,6 +30,17 @@ public class UserService {
     public void delete(String id) {
         findById(id);
         repository.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 
     public User fromDTO(UserDTO objDto) {
