46c57b6d82b644ec1e3b64a3c9d95c0721f0879b
claudio-Antonio <claudioanery@gmail.com>
2025-11-15 13:14:06 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/claudioantonio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/claudioantonio/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.claudioantonio.workshopmongo.resources;
 
+import com.claudioantonio.workshopmongo.domain.Post;
 import com.claudioantonio.workshopmongo.domain.User;
 import com.claudioantonio.workshopmongo.dto.UserDTO;
 import com.claudioantonio.workshopmongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
