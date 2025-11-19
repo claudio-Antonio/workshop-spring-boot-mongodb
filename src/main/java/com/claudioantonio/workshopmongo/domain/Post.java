52b5f5699335607a17499d2651a2825484e91e93
claudio-Antonio <claudioanery@gmail.com>
2025-11-19 08:46:36 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/claudioantonio/workshopmongo/domain/Post.java
+++ src/main/java/com/claudioantonio/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.claudioantonio.workshopmongo.domain;
 
 import com.claudioantonio.workshopmongo.dto.AuthorDTO;
+import com.claudioantonio.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -15,7 +18,9 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
+
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {}
 
@@ -65,6 +70,14 @@ public class Post implements Serializable {
 
     public void setAuthorDTO(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
